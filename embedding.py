7af7c55ceb4bacb011f29d142c3511f83f252b69
Gianlucca Colangelo <gianlucisnt@uba.ar>
2024-07-25 15:36:37 -0300

    first version of embedding.py already working

--- embedding.py
+++ embedding.py
@@ -1,9 +1,13 @@
 import torch
 import torch.nn as nn
+import pickle
+from typing import List
 
 
 class HPOEmbedder:
-    def __init__(self, embedding_weights_path, device=None):
+    def __init__(
+        self, embedding_weights_path="pre_trained_embedding_weights.pth", device=None
+    ):
         # Determine the device
         self.device = (
             device if device else ("cuda" if torch.cuda.is_available() else "cpu")
@@ -23,9 +27,26 @@ class HPOEmbedder:
         self.embedding_layer.weight = nn.Parameter(embedding_weights)
         self.embedding_layer.to(self.device)
 
-    def get_embedding(self, input_indices):
-        # Ensure the input is a tensor
-        input_tensor = torch.tensor(input_indices, dtype=torch.long).to(self.device)
+        # Open phenotypes vocabulary
+        with open("phen_vocabulary.pkl", "rb") as file:
+            self.phen_vocabulary = pickle.load(file)
+
+    def _phen_to_idx(self, phen_list: List[str]) -> torch.Tensor:
+        """
+        This function convert any list of phens to a tensor of indices
+        ready to feed the embedding layer!
+        """
+        indices = [self.phen_vocabulary[phenotype] for phenotype in phen_list]
+        return torch.Tensor(indices).type(torch.long)
+
+    def get_embedding(self, phen_list: List[str]) -> torch.Tensor:
+        # Convert phenotype list to tensor of indices
+        input_indices = self._phen_to_idx(phen_list).to(self.device)
         # Get the embedding
-        embedding = self.embedding_layer(input_tensor)
+        embedding = self.embedding_layer(input_indices)
         return embedding
+
+## TODO:
+[] que no trackee gradiente
+[] preguntar por otras optimizaciones del estilo
+[] que el output sea un tensor en numpy
